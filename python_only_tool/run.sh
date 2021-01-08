b21ceb40149b7979a1b742d1e11f36dcb4eceef8
Paul Ranum <paulranum11@gmail.com>
2021-01-08 11:10:43 -0600

    Update to support alignment

--- python_only_tool/run.sh
+++ python_only_tool/run.sh
@@ -1,3 +1,3 @@
-python splitseqdemultiplex_0.2.2.py -n 10 -e 1 -m 10 -1 Round1_barcodes_new5.txt -2 Round2_barcodes_new4.txt -3 Round3_barcodes_new4.txt \
-	-f SRR6750041_1_smalltest.fastq -r SRR6750041_2_smalltest.fastq -o output -t 8000 -a True -x missing -b 100000 -p True -l 20000	
+python splitseqdemultiplex_0.2.2.py -n 15 -e 1 -m 10 -1 Round1_barcodes_new5.txt -2 Round2_barcodes_new4.txt -3 Round3_barcodes_new4.txt \
+	-f ~/Desktop/WK14_S2_R1_001_head.fastq -r ~/Desktop/WK14_S2_R2_001_head.fastq -o output -t 8000 -a True -x missing -b 100000 -p True -l 3000000	
 
