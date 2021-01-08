60982c42761e7cc395bee19926ab1c5034b8e99b
Paul Ranum <paulranum11@gmail.com>
2021-01-08 12:13:08 -0600

    update options

--- python_only_tool/run.sh
+++ python_only_tool/run.sh
@@ -1,3 +1,16 @@
-python splitseqdemultiplex_0.2.2.py -n 15 -e 1 -m 10 -1 Round1_barcodes_new5.txt -2 Round2_barcodes_new4.txt -3 Round3_barcodes_new4.txt \
-	-f ~/Desktop/WK14_S2_R1_001_head.fastq -r ~/Desktop/WK14_S2_R2_001_head.fastq -o output -t 8000 -a True -x missing -b 100000 -p True -l 3000000	
+python splitseqdemultiplex_0.2.2.py \
+	-n 15 \
+	-e 1 \
+	-m 10 \
+	-1 Round1_barcodes_new5.txt \
+	-2 Round2_barcodes_new4.txt \
+	-3 Round3_barcodes_new4.txt \
+	-f ~/Desktop/WK14_S2_R1_001_head.fastq \
+	-r ~/Desktop/WK14_S2_R2_001_head.fastq \
+	-o output \
+	-a False \
+	-x missing \
+	-b 100000 \
+	-p True \
+	-l 3000000	
 
