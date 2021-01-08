ff39e3331cba6c06732a87c64538e629334fc247
Paul Ranum <paulranum11@gmail.com>
2021-01-08 11:36:03 -0600

    remove file

--- python_only_tool/run1.sh
+++
@@ -1,3 +0,0 @@
-python splitseqdemultiplex_0.2.2.py -n 12 -e 1 -m 10 -1 Round1_barcodes_new5.txt -2 Round2_barcodes_new4.txt -3 Round3_barcodes_new4.txt \
-	-f SRR6750041_1.fastq -r SRR6750041_2.fastq -o output -g 1000 -t 8000 -a True -x missing -b 100000 -p True -l 310484724	
-
