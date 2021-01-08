b9bd82cdb067c59ac3227d242c77a76db2f7be0f
Ranum <ranump@c-2-01.cm.cluster>
2021-01-08 15:37:28 -0500

    updated run.sh

--- python_only_tool/run.sh
+++ python_only_tool/run.sh
@@ -1,16 +1,17 @@
 python splitseqdemultiplex_0.2.2.py \
-	-n 15 \
+	-n 4 \
 	-e 1 \
 	-m 10 \
 	-1 Round1_barcodes_new5.txt \
 	-2 Round2_barcodes_new4.txt \
 	-3 Round3_barcodes_new4.txt \
-	-f ~/Desktop/WK14_S2_R1_001_head.fastq \
-	-r ~/Desktop/WK14_S2_R2_001_head.fastq \
+	-f SRR6750041_1_smalltest.fastq \
+	-r SRR6750041_2_smalltest.fastq \
 	-o output \
-	-a False \
-	-x missing \
-	-b 100000 \
+	-a True \
+	-x /mnt/isilon/davidson_lab/ranum/Tools/STAR_Genomes/mm10 \
+	-s GRCm38_genes.saf \
+        -b 1000 \
 	-p True \
-	-l 3000000	
+	-l 20000 
 
