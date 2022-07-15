3265ef943c24296265be2f02050597cef172e80e
Ranum <paulranum11@gmail.com>
2022-07-15 12:12:02 -0400

    make BC postion finding optional

--- python_only_tool/run.sh
+++ python_only_tool/run.sh
@@ -5,20 +5,24 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=50G
 
-python splitseqdemultiplex_0.2.2.py \
+python splitseqdemultiplex_0.2.3.py \
 	-n 10 \
 	-e 1 \
-	-m 1 \
+	-m 2 \
 	-1 Round1_barcodes_new5.txt \
 	-2 Round2_barcodes_new4.txt \
 	-3 Round3_barcodes_new4.txt \
-	-f SRR6750041_1_smalltest.fastq \
-	-r SRR6750041_2_smalltest.fastq \
+	-f /mnt/isilon/davidson_lab/ranum/SequencingAnalysis/HD_S2_AllSamples/HD_S2_1A_S9_R1_001_adapterTrimFinal_head.fastq \
+	-r /mnt/isilon/davidson_lab/ranum/SequencingAnalysis/HD_S2_AllSamples/HD_S2_1A_S9_R2_001_adapterTrimFinal_head.fastq \
 	-o output \
 	-a True \
 	-x /mnt/isilon/davidson_lab/ranum/Tools/STAR_Genomes/mm10 \
 	-s GRCm38_genes.saf \
-        -b 1000 \
+        -b 10000 \
 	-p True \
-	-l 20000 
+	-k \
+	-l 100000
+
+
+#1641582384
 
